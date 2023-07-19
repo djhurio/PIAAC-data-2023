a251640a35080a5fac207410ea538d6e90a91e91
Mārtiņš Liberts <martins.liberts@gmail.com>
2023-07-19 07:17:59 +0300

    var names toupper

--- code/50-sdif-create.R
+++ code/50-sdif-create.R
@@ -71,6 +71,8 @@ dat_sdif[, sum(theor_pbwt, na.rm = T), keyby = .(weightflg)]
 
 # Save
 
+setnames(dat_sdif, toupper)
+
 fwrite(x = dat_sdif, file = "result/CY2_Final_SDIF_LVA.csv",  yaml = F)
 fwrite(x = dat_sdif, file = "result/CY2_Final_SDIF_LVA.csvy", yaml = T)
 openxlsx::write.xlsx(x = dat_sdif, file = "result/CY2_Final_SDIF_LVA.xlsx",
