669885f46e55477545259decf00281084cf6f6c3
Mārtiņš Liberts <martins.liberts@gmail.com>
2024-01-07 04:23:13 +0200

    w6 and wif - done

--- code/70-WeightingQCForm6.R
+++ code/70-WeightingQCForm6.R
@@ -67,17 +67,27 @@ variable.format <- function(DT, varname) {
 # WeightingQCForm-6_FinalWt_LVA.xlsx
 
 # SDIF - final
-dat_sdif <- fread(
-  file = "result/CY2_Final_SDIF_LVA.csvy",
-  yaml = TRUE
-)
-dat_sdif[, .N]
+dat_sdif <- haven::read_sas(
+  data_file = "data-weights/SAS/SAS7BDAT/psdlvams.sas7bdat"
+) |> setDT()
 
-# WIF_QCChecks
+# WIF
 dat_wif <- haven::read_sas(
   data_file = "data-weights/WIF_QCChecks_LVA.sas7bdat"
 ) |> setDT()
-dat_wif[, .N]
+
+# SPRWT0
+dat_sprwt <- haven::read_sas(
+  data_file = "data-weights/sprwt.sas7bdat"
+) |> setDT()
+
+dat_wif <- merge(
+  x = dat_wif,
+  y = dat_sprwt,
+  by = "PERSID",
+  all.x = TRUE
+)
+rm(dat_sprwt)
 
 # BQ
 dat_bq <- readRDS(file = "data/dat_bq.rds")
@@ -165,7 +175,7 @@ dat_pers[, .N, keyby = .(
 tab1_data <- dat_pers[WEIGHTFLG == 1, .(
   CASEID, PERSID,
   HHBWT0, HHUEWT0, HHNRWT0,
-  SPBWT0, SPUEWT0, SPNRWT0, SPLNRWT0, SPRWT0 = NA_real_, SPTWT0, SPFWT0
+  SPBWT0, SPUEWT0, SPNRWT0, SPLNRWT0, SPRWT0, SPTWT0, SPFWT0
 )][order(-SPFWT0)][1:20] |> melt.data.table(id.vars = c("CASEID", "PERSID"))
 
 tab1_test <- openxlsx2::read_xlsx(
@@ -190,17 +200,12 @@ tab1 <- merge(
   by = c("CASEID", "PERSID", "id")
 )
 
-tab1[
-  -grep("SPRWT0", variable.x),
-  .(CASEID, PERSID, variable.x, value.x, value.y)
-]
+tab1[, .(CASEID, PERSID, variable.x, value.x, value.y)]
 
 # SPRWT0 is not available in WIF
-if (!isTRUE(tab1[
-  -grep("SPRWT0", variable.x),
-  all.equal(value.x, value.y, check.attributes = F)
-])) stop("Check Table 1")
-
+if (!isTRUE(tab1[, all.equal(value.x, value.y, check.attributes = F)])) {
+  stop("Check Table 1")
+}
 
 
 # Table 2
@@ -208,18 +213,28 @@ dat_pers[, .N, keyby = .(GENDER_R)]
 dat_pers[, sprintf("%.2f", sum(SPFWT0))]
 dat_pers[, sprintf("%.2f", sum(SPFWT0)), keyby = .(GENDER_R)]
 
+dat_pers[, y_gender := factor(
+  x = GENDER_R,
+  levels = 1:2,
+  labels = c("1 Male", "2 Female")
+)]
+
 dat_pers[, .N, keyby = .(AGE_R)]
 dat_pers[, sprintf("%.2f", sum(SPFWT0))]
 dat_pers[, sprintf("%.2f", sum(SPFWT0)), keyby = .(AGE_R)]
 
-dat_pers[, age_group := trunc((AGE_R - 6) / 10)]
+dat_pers[AGE_R %in% 16:65, age_group := trunc((AGE_R - 6) / 10)]
+dat_pers[AGE_R > 65, age_group := 5]
 dat_pers[, .(.N, Min = min(AGE_R), Max = max(AGE_R)), keyby = .(age_group)]
 dat_pers[, sprintf("%.2f", sum(SPFWT0)), keyby = .(age_group)]
 
-dat_pers[, sprintf("%.2f", sum(SPFWT0)), keyby = .(RAKEDIM1)]
-dat_pers[, sprintf("%.2f", sum(SPFWT0)), keyby = .(RAKEDIM2)]
-dat_pers[, sprintf("%.2f", sum(SPFWT0)), keyby = .(RAKEDIM3)]
+dat_pers[, y_age := factor(
+  x = age_group,
+  levels = 1:5,
+  labels = c("16-25", "26-35", "36-45", "46-55", "56-65")
+)]
 
+dat_pers[, .N, keyby = .(y_age)]
 
 
 # Recode b2_q01lv to ISCED
@@ -232,6 +247,7 @@ dat_bq[
   )
 ]
 dat_bq[, .N, keyby = .(y_edu, b2_q01lv)]
+dat_bq[, .N, keyby = .(y_edu)]
 
 
 # Country of birth
@@ -241,7 +257,8 @@ dat_bq[, y_natb := factor(
   levels = 1:2,
   labels = c("1 Yes", "2 No")
 )]
-dat_bq[, .N, keyby = .(a2_q03a, y_natb)]
+dat_bq[, .N, keyby = .(y_natb, a2_q03a)]
+dat_bq[, .N, keyby = .(y_natb)]
 
 
 # Economic activity
@@ -253,17 +270,18 @@ dat_bq[, y_ecact := factor(
   levels = 1:3,
   labels = c("1 Employed", "2 Unemployed", "3 Out of the labor force")
 )]
+dat_bq[, .N, keyby = .(y_ecact)]
 
 
+
+# Merge BQ to data
 dat_bq[, .(persid, y_edu, y_natb, y_ecact)]
 
 dat_pers <- merge(
   x = dat_pers,
-  y = dat_bq[, .(persid, y_edu, y_natb, y_ecact)],
-  by.x = "PERSID",
-  by.y = "persid",
-  all.x = TRUE,
-  sort = FALSE
+  y = dat_bq[, .(PERSID = persid, y_edu, y_natb, y_ecact)],
+  by = "PERSID",
+  all.x = TRUE
 )
 
 
@@ -271,21 +289,215 @@ dat_pers[, .(.N, sum(SPFWT0)), keyby = .(y_edu)]
 dat_pers[, .(.N, sum(SPFWT0)), keyby = .(y_natb)]
 dat_pers[, .(.N, sum(SPFWT0)), keyby = .(y_ecact)]
 
-grep("^SPFWT[1-9][0-9]?$", names(dat_pers), value = TRUE) |> length()
-grep("^y_", names(dat_pers), value = TRUE)
+dat_pers[, y_rakedim1 := factor(
+  x = RAKEDIM1,
+  levels = sort(unique(RAKEDIM1)),
+  labels = sprintf("%02d", unique(sort(RAKEDIM1)))
+)]
+dat_pers[, y_rakedim2 := factor(
+  x = RAKEDIM2,
+  levels = sort(unique(RAKEDIM2)),
+  labels = sprintf("%02d", sort(unique(RAKEDIM2)))
+)]
+dat_pers[, y_rakedim3 := factor(
+  x = RAKEDIM3,
+  levels = sort(unique(RAKEDIM3)),
+  labels = sprintf("%02d", sort(unique(RAKEDIM3)))
+)]
 
-dat_varest <- melt.data.table(
-  data = dat_pers,
-  id.vars = c("CASEID", "PERSID", "SPFWT0",
-              grep("^y_", names(dat_pers), value = TRUE)),
-  measure.vars = grep("^SPFWT[1-9][0-9]?$", names(dat_pers), value = TRUE),
-  variable.name = "REP",
-  value.name = "REPWGT"
-) |> melt.data.table(
-  id.vars = c("REP", "CASEID", "PERSID", "SPFWT0", "REPWGT"),
+
+
+
+dat_pers[, sprintf("%.2f", round(sum(SPFWT0))), keyby = .(y_gender)]
+dat_pers[, sprintf("%.2f", round(sum(SPFWT0))), keyby = .(y_age)]
+dat_pers[, sprintf("%.2f", round(sum(SPFWT0))), keyby = .(y_rakedim1)]
+dat_pers[, sprintf("%.2f", round(sum(SPFWT0))), keyby = .(y_rakedim2)]
+dat_pers[, sprintf("%.2f", round(sum(SPFWT0))), keyby = .(y_rakedim3)]
+
+
+
+grep("^SPFWT[1-9][0-9]?$", names(dat_pers), value = TRUE) |> length()
+
+# g <- grep("^SPFWT[1-9][0-9]?$", names(dat_pers)) |> length()
+# k <- 0.3 # from WeightingQCForm-2_BaseWeights_Replicates_LVA
+# 1 / (g * (1 - k) ^ 2)
+
+# y <- "y_gender"
+# dt <- copy(dat_pers)
+
+est_var <- function(y, dt = dat_pers, k = 0.3) {
+
+  # Full sample estimates
+  dat_var_full <- dt[
+    ,
+    .(samp_size = .N,
+      weight_freq = as.integer(round(sum(SPFWT0)))),
+    keyby = c(y)
+  ]
+  
+  dat_var_repl <- melt.data.table(
+    data = dt,
+    id.vars = c("CASEID", "PERSID", y),
+    measure.vars = grep("^SPFWT[1-9][0-9]?$", names(dat_pers), value = TRUE),
+    variable.name = "REPLICATE",
+    value.name = "REPWGT"
+  )[
+    ,
+    .(samp_size_rep = .N,
+      weight_freq_rep = as.integer(round(sum(REPWGT)))),
+    keyby = c("REPLICATE", y)
+  ]
+
+  dat_var <- merge(
+    x = dat_var_repl,
+    y = dat_var_full,
+    by = y,
+    all = TRUE
+  )
+  
+  g <- dat_var[, length(unique(REPLICATE))]
+  
+  dat_var <- dat_var[
+    ,
+    .(se = sqrt(sum((weight_freq_rep - weight_freq) ^ 2) / g / (1 - k) ^ 2)),
+    keyby = c(y)
+  ]
+  
+  dat_var_res <- merge(
+    x = dat_var_full,
+    y = dat_var,
+    all = TRUE
+  )
+  
+  dat_var_res[, variable := y]
+  dat_var_res[, cv := se / weight_freq * 100]
+  setnames(dat_var_res, y, "value")
+  setcolorder(dat_var_res, c("variable", "value"))
+  
+  return(dat_var_res[])
+}
+
+y_variables <- grep("^y_", names(dat_pers), value = TRUE)
+y_variables
+
+tab_w6_3 <- map_dfr(.x = y_variables, .f = est_var)
+openxlsx2::write_xlsx(
+  x = tab_w6_3,
+  file = "WeightingQCForms/tab_w6_3.xlsx"
 )
 
-dat_varest
-str(dat_varest)
+dat_pers[, .(.N, WGT_SUM = sprintf("%.2f", sum(SPFWT0)))]
+dat_pers[, .(.N, WGT_SUM = sprintf("%.2f", sum(SPFWT0))), keyby = .(DISP_CIBQ)]
 
-dat_varest
+
+# Weighting International File (WIF) for QC LVA ####
+# https://piaac.ets.org/portal/weighting-international-file-wif-for-qc-lva/
+
+# Look at the characteristics of persons with large weights.
+# Analysts should be aware of the potential influence that cases with
+# large weights can have on their analyses, especially when extreme weights are
+# associated with extreme data points.
+# 
+# Some trimming was performed, but it was kept at a minimum as it can
+# introduce bias into the estimates. You may also want to look at subgroups
+# that may be of interest in analysis to check for any outliers within the subgroup
+
+dat_pers[
+  , .(CASEID, PERSID, SPFWT0, GENDER_R, AGE_R, REGION, y_edu, y_ecact)
+][order(-SPFWT0)][1:20]
+
+
+# As a check on the final weights, the QC forms should include estimates
+# of key BQ variables, but please note that the estimates
+# for education attainment, native born and employment status are
+# left blank in Table 3 of W-6 since we do not have access to the BQ data.
+# 
+# We encourage you to use your BQ data and the final weights (SPFWT0)
+# on the QC WIF to compute estimates for the three variables and return
+# W-6 form to us after you complete Table 3.
+# 
+# We also recommend that you use the final weights to produce
+# other estimates of interest and check them for reasonableness.
+
+# Jāsaprot, ko nozīmē papildus novērtējumi.
+# IZM (pasūtītāju) varētu interesēt reģionālais, valodu un vecuma grupu griezums.
+# Redzot, kāds ir profesiju kodēšanas rezultāts, neredzu, ka tur varētu sanākt
+# arī profesiju grupu griezums (mums bija jākodē ar 4 ciparu precizitāti).
+# Jāpiebilst, ka mēs arī esam ieguvuši krievvalodīgos mazāk nekā plānots.
+# Pavisam maz ir krievvalodīgo jauniešu.
+
+est_var("REGION")
+
+# Kāda ir valoda, kuru Jūs pirmo reizi apguvāt bērnībā, mājās, UN JOPROJĀM SAPROTAT?
+dat_bq[, .N, keyby = .(a2_q04a1lv)]
+
+# Kādā valodā Jūs visbiežāk runājat mājās?
+dat_bq[, .N, keyby = .(a2_q04blv)]
+
+dat_pers <- merge(
+  x = dat_pers,
+  y = dat_bq[, .(PERSID = persid, a2_q04a1lv, a2_q04blv)],
+  by = "PERSID",
+  all.x = TRUE
+)
+
+dat_pers[, region := factor(x = REGION, levels = 1:6, labels = c(
+  "Rīgas statistiskais reģions",
+  "Pierīgas statistiskais reģions",
+  "Vidzemes statistiskais reģions",
+  "Kurzemes statistiskais reģions",
+  "Zemgales statistiskais reģions",
+  "Latgales statistiskais reģions"
+))]
+
+dat_pers[, valoda_pirma_a2_q04a1lv := factor(
+  x = a2_q04a1lv,
+  levels = 1:9,
+  labels = c(
+    "Latviešu",
+    "Krievu",
+    "Ukraiņu",
+    "Baltkrievu",
+    "Poļu",
+    "Angļu",
+    "Igauņu",
+    "Vācu",
+    "Cita valoda"
+  )
+)]
+
+dat_pers[, valoda_majas_a2_q04blv := factor(
+  x = a2_q04blv,
+  levels = 1:7,
+  labels = c(
+    "Latviešu",
+    "Krievu",
+    "Ukraiņu",
+    "Baltkrievu",
+    "Poļu",
+    "Angļu",
+    "Cita valoda"
+  )
+)]
+
+dat_pers[, .N, keyby = .(region)]
+dat_pers[, .N, keyby = .(valoda_pirma_a2_q04a1lv)]
+dat_pers[, .N, keyby = .(valoda_majas_a2_q04blv)]
+
+tab_est <- map_dfr(
+  .x = c(
+    "region",
+    "valoda_pirma_a2_q04a1lv",
+    "valoda_majas_a2_q04blv"
+  ),
+  .f = est_var
+)
+
+tab_est <- tab_est[!is.na(value) & !value %in% 97:98]
+tab_est
+
+openxlsx2::write_xlsx(
+  x = tab_est,
+  file = "WeightingQCForms/tab_WIF-estimates_extra.xlsx",
+  as_table = TRUE
+)
